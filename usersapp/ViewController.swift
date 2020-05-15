8293340557f99b028f9d3df0c2268bccfa781581
xredlinex <redik.line@gmail.com>
2020-05-15 14:49:43 +0300

    initital commit

---
+++ usersapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  usersapp
+//
+//  Created by alexey sorochan on 15.05.2020.
+//  Copyright © 2020 alexey sorochan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
