6ac4e4f4d9aede36bb2292e19325cb1d090df0ff
xredlinex <redik.line@gmail.com>
2020-05-19 11:26:57 +0300

    user info controller

---
+++ usersapp/UserInfoContrioller/DetailInfoViewController+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  DetailInfoViewController+Extension.swift
+//  usersapp
+//
+//  Created by alexey sorochan on 19.05.2020.
+//  Copyright © 2020 alexey sorochan. All rights reserved.
+//
+
+import Foundation
---
+++ usersapp/UserInfoContrioller/DetailInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailInfoViewController.swift
+//  usersapp
+//
+//  Created by alexey sorochan on 19.05.2020.
+//  Copyright © 2020 alexey sorochan. All rights reserved.
+//
+
+import UIKit
+
+class DetailInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
