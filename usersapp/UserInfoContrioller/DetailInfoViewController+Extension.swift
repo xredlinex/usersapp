e29036c56a53d1f7546b8ce35356e597b3651e4e
xredlinex <redik.line@gmail.com>
2020-05-19 12:15:36 +0300

    user info controller

--- usersapp/UserInfoContrioller/DetailInfoViewController+Extension.swift
+++ usersapp/UserInfoContrioller/DetailInfoViewController+Extension.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2020 alexey sorochan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import MessageUI
+
+extension DetailInfoViewController {
+    
+    func setupBackground() {
+           let colorOne = UIColor(red: 95/255, green: 96/255, blue: 100/255, alpha: 1).cgColor
+           let colorTwo = UIColor(red: 40/255, green: 41/255, blue: 45/255, alpha: 1).cgColor
+           self.view.setupBackGroundGradient([colorTwo, colorOne])
+       }
+}
+
+
+
+
+
+
+
+
+
+
+
+extension DetailInfoViewController {
+    
+   
+}
